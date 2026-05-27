7d9183d43b4ffdcfd090b54f52a87509a3eef967
yohannes <yohannesg218@gmail.com>
2026-05-27 10:09:24 -0700

    fix schema error

--- backend/src/email/email.module.ts
+++ backend/src/email/email.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 
+import { PrismaModule } from '../../prisma/prisma.module';
+
 @Module({
+  imports: [
+    PrismaModule,
+
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      },
+
+      defaults: {
+        from: `"No Reply" <${process.env.EMAIL_USER}>`,
+      },
+    }),
+  ],
+
   controllers: [EmailController],
+
   providers: [EmailService],
+
+  exports: [EmailService],
 })
 export class EmailModule {}
